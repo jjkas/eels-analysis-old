c291f4f5ef528dbafe171f748b29a78c9dd46733
jjkas <joshua.j.kas@gmail.com>
2019-12-10 10:48:01 -0800

    Changed input variables to match DM 2.3 for comparison.

--- nion/eels_analysis/test/stoichiometry_test.py
+++ nion/eels_analysis/test/stoichiometry_test.py
@@ -60,32 +60,31 @@ class TestLibrary(unittest.TestCase):
     def test_stoichiometry_found_from_experimental_eels(self):
         # Read EELS data from file (BN). This is data from Tracy, taken from a thin part of the sample,
         # and represents to some extent a best case scenario.
-        #data_file = Path('./Test_Data/eelsdbBN.dat')
+        data_file = Path('./Test_Data/eelsdbBN.csv')
         #data_file = Path('./Test_Data/EELS_Thick.csv')
-        data_file = Path('./Test_Data/EELS_Thin.csv')
+        #data_file = Path('./Test_Data/EELS_Thin.csv')
         
         energy_grid,spectrum = numpy.loadtxt(data_file, delimiter=',',unpack=True)
         
         # Set up input to stoichiometry quantification. All settings are hard coded to
-        # BN from the EELS Atlas.
+        # BN to match DM 2.32.888.
         atomic_numbers=[5,7]
         edge_label = 'K'
         beam_energy_keV = 200.0
         convergence_angle_mrad = 0.0
         collection_angle_mrad = 100.0     
-        edge_onsets = [193.8, 402.4]
-        edge_deltas = [30.0, 30.0]
+        edge_onsets = [188.0, 401.0]
+        edge_deltas = [25.0, 25.0]
         background_ranges = [numpy.zeros(2),numpy.zeros(2)]
-        iEdge=0
-        for edge_onset in edge_onsets:
-            background_ranges[iEdge][0] = edge_onset - 30.0
-            background_ranges[iEdge][1] = edge_onset - 10.0
-            iEdge += 1
+        background_ranges[0][0] = 167.0
+        background_ranges[0][1] = 183.0
+        background_ranges[1][0] = 358.0
+        background_ranges[1][1] = 393.0
             
         erange = numpy.zeros(2)
         erange[0] = energy_grid[0]
         erange[1] = energy_grid[-1]
-        print(background_ranges,erange)
+        
         stoichiometry = EELS_DataAnalysis.stoichiometry_from_eels(spectrum,erange,background_ranges,atomic_numbers,edge_onsets,edge_deltas,
                                                 beam_energy_keV*1000.0, convergence_angle_mrad/1000.0, collection_angle_mrad/1000.0)
         
